71a66b7039de6d781456d0a37b865d4e19c59e30
Jan <cloux@rote.ch>
2019-03-08 21:18:35 +0100

    fix: resource leaks and other issues as reported by infer
     to run infer static code analyzer (fbinfer.com):
     cd src; infer run -- make
    
     - fix: fd_move.c resource leak - close file handle
     - fix: runit-init.c resource leak - close file handle
     - fix: svlogd.c dead storage - value never used
     - fix: x86cpuid.c uninitialized value read
     - fix: package/install-man - do not use unreliable symlinks

--- admin/runit-2.1.2/src/svlogd.c
+++ admin/runit-2.1.2/src/svlogd.c
@@ -396,7 +396,7 @@ unsigned int ip4_scan(const char *s,char ip[4])
   if (*s != '.') return 0; ++s; ++len;
   i = scan_ulong(s,&u); if (!i) return 0; ip[2] = u; s += i; len += i;
   if (*s != '.') return 0; ++s; ++len;
-  i = scan_ulong(s,&u); if (!i) return 0; ip[3] = u; s += i; len += i;
+  i = scan_ulong(s,&u); if (!i) return 0; ip[3] = u; len += i;
   return len;
 }
 
