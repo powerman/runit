a9089b13b852a796cd0584b53f3555f9d8addac4
Z. Liu <zhixu.liu@gmail.com>
2024-08-25 03:30:40 +0800

    fix -Wpointer-sign
    found by using clang 18 with CFLAGS: -Wall -Wextra
    
    prot.c:15:19: warning: passing 'int *' to parameter of type 'const __gid_t *' (aka 'const unsigned int *') converts between pointers to integer types with different sign [-Wpointer-sign]
       15 |   if (setgroups(1,&gid) == -1) return -1;
          |                   ^~~~

--- admin/runit-2.1.2/src/prot.c
+++ admin/runit-2.1.2/src/prot.c
@@ -5,14 +5,16 @@
 #include "hasshsgr.h"
 #include "prot.h"
 
+#ifdef HASSHORTSETGROUPS
+int prot_gid(gid_t gid)
+{
+  if (setgroups(1,&gid) == -1) return -1;
+#else
 int prot_gid(int gid)
 {
-#ifdef HASSHORTSETGROUPS
   short x[2];
   x[0] = gid; x[1] = 73; /* catch errors */
   if (setgroups(1,x) == -1) return -1;
-#else
-  if (setgroups(1,&gid) == -1) return -1;
 #endif
   return setgid(gid); /* _should_ be redundant, but on some systems it isn't */
 }
--- admin/runit-2.1.2/src/prot.h
+++ admin/runit-2.1.2/src/prot.h
@@ -3,7 +3,13 @@
 #ifndef PROT_H
 #define PROT_H
 
+#include "prot.h"
+
+#ifdef HASSHORTSETGROUPS
+extern int prot_gid(gid_t);
+#else
 extern int prot_gid(int);
+#endif
 extern int prot_uid(int);
 
 #endif
